b019127ff373ffe6a99a4b2b15a523a934fcde11
Andres Garcia Sanchez <force.rap20@icloud.com>
2024-11-28 15:39:26 -0600

    server.js

--- server.js
+++ server.js
@@ -37,6 +37,6 @@ app.get('/profile/:id', (req, res) => profile.handleProfileGet(req, res, db));
 app.put('/image', (req, res) => image.handleImage(req, res, db));
 app.post('/imageurl', (req, res) => image.handleApiCall(req, res)); // Clarifai API call route
 
-app.listen(process.env.PORT || 3001, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log(`App is running on port ${process.env.PORT}`);
 });
